e19151fc23ac00d369b90550ffc367fc313a8ad3
VuxNT <vunt1809@gmail.com>
2024-04-01 11:07:08 +0700

    update dich vu

--- src/main/java/vn/com/gsoft/services/config/PortCustomizerConfiguration.java
+++ src/main/java/vn/com/gsoft/services/config/PortCustomizerConfiguration.java
@@ -19,7 +19,7 @@ public class PortCustomizerConfiguration implements WebServerFactoryCustomizer<C
     @Override
     public void customize(ConfigurableWebServerFactory factory) {
         int port = findRandomOpenPort(portRange[0], portRange[1]);
-        factory.setPort(191);
+        factory.setPort(port);
         System.setProperty("server.port", String.valueOf(port));
     }
 
